cf7d3b60537b8b40125c13d6dd87d07343313f5e
cristopher santana <cristopher.santana@tecnologicoliceocristiano.edu.ec>
2024-08-01 18:02:44 -0500

    Sumarlista

---
+++ Sumarlista.py
@@ -0,0 +1,6 @@
+# Objetivo: Practicar la iteración y suma de elementos en una lista.
+# Instrucciones: Dada una lista de números, escribe un programa que calcule la suma de todos los elementos en la lista y la imprima.
+
+lista = [12 , 2 , 4 , 14 , 22 , 34 , 19]
+suma = sum(lista)
+print(suma)
